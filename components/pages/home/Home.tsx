55835c8c43dfc5e1c1504107340a4feb0e8e9318
James F. Thomas <57680469+JamesFThomas@users.noreply.github.com>
2026-02-12 09:33:28 -0600

    docs(readme): expand upcoming features with infrastructure and platform roadmap

---
+++ components/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+export const HomePage = () => {
+  return (
+    <main
+      id='Homepage-container'
+      className='flex flex-1 flex-col items-center justify-center px-16 bg-white dark:bg-black'
+    >
+      <h1 className='text-5xl font-extrabold text-zinc-900 dark:text-white sm:text-6xl'>
+        Home Page
+      </h1>
+    </main>
+  );
+};
